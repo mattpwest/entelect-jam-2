e1d59202e37dd5ba4b7dd934139e411bfcb86e78
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-03-18 15:18:11 +0200

    Added a loss screen (loss from running out of time). Added a coin powerup that grants extra time.

---
+++ Assets/Scripts/TimePowerup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePowerup : MonoBehaviour {
+
+    private Timer Timer;
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        // Cheap & dirty - if anything hits us add time
+        Timer.AddTime(0, 15);
+        Destroy(this.gameObject);
+    }
+
+    void Start () {
+        Timer = FindObjectOfType<Timer>();
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public int Minutes = 0;
+    public int Seconds = 15;
+
+    private Text timeLeftText;
+
+    private int TotalSeconds = 0;
+    private float TimeAccumulated = 0.0f;
+
+	void Start () {
+        timeLeftText = GetComponent<Text>();
+
+        TotalSeconds = Minutes * 60 + Seconds;
+	}
+	
+	void Update () {
+        Debug.Log(TotalSeconds);
+        TimeAccumulated += Time.deltaTime;
+
+        if (TimeAccumulated >= 1.0f) {
+            TotalSeconds--;
+            TimeAccumulated -= 1.0f;
+        }
+
+        if (TotalSeconds <= 0) {
+            SceneManager.LoadScene("Lost");
+        }
+
+        int minutes = TotalSeconds / 60;
+        int seconds = TotalSeconds % 60;
+        string text = minutes + ":";
+        if (seconds >= 10) {
+            text += seconds;
+        } else {
+            text += "0" + seconds;
+        }
+        timeLeftText.text = text;
+	}
+
+    public void AddTime(int minutes, int seconds) {
+        TotalSeconds += minutes * 60 + seconds;
+    }
+}
