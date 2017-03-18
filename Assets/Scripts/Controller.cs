971ca3fd8ddd648b76aa5100f1dc39d7869c47d2
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-03-18 14:27:31 +0200

    Basic win condition implemented (get out the door). Ladders implemented.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,22 +1,31 @@
 ﻿using UnityEngine;
+using System;
 
 public class Controller : MonoBehaviour
 {
     public float MaxSpeed = 10.0f;
     public float JumpModifier = 5.0f;
-    private float horizontalInput;
-    private Rigidbody2D rigidbody2D;
-    private bool jumpButtonPressed;
-    private Animator animator;
-    private int lastDirection = 1;
-    private Transform groundSensor;
+    public float LadderUpSpeed = 3.0f;
+    public float LadderDownSpeed = 6.0f;
     public LayerMask groundLayer;
+    public LayerMask ladderLayer;
+
+    private Rigidbody2D body;
+    private Animator animator;
+    private Transform groundSensor;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private bool jumpButtonPressed;
+
+    private bool ladderMounted = false;
+    private int lastDirection = 1;
 
     // Use this for initialization
 	private void Start ()
 	{
         this.animator = this.gameObject.GetComponent<Animator>();
-	    this.rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+	    this.body = this.gameObject.GetComponent<Rigidbody2D>();
         this.groundSensor = this.gameObject.transform.FindChild("GroundSensor");
 	}
 	
@@ -24,32 +33,62 @@ public class Controller : MonoBehaviour
 	private void Update ()
 	{
 	    this.horizontalInput = Input.GetAxis("Horizontal");
-
+        this.verticalInput = Input.GetAxis("Vertical");
 	    this.jumpButtonPressed = Input.GetButtonDown("Jump");
 	}
 
     //FixedUpdate is called every fixed framerate frame
     private void FixedUpdate()
     {
-        var speed = this.MaxSpeed * this.horizontalInput;
-        var moveSpeed = new Vector2(speed, this.rigidbody2D.velocity.y);
-        // checks if you are within 0.15 position in the Y of the ground
-        var grounded = Physics2D.OverlapCircle(groundSensor.transform.position, 0.15f, groundLayer) != null;
-        //Debug.Log(grounded);
-
-        this.rigidbody2D.velocity = moveSpeed;
-
-        if (this.jumpButtonPressed && grounded)
-        {
-            var jumpForce = new Vector2(0, this.JumpModifier);
-
-            this.rigidbody2D.AddForce(jumpForce, ForceMode2D.Impulse);
+        var speedX = this.MaxSpeed * this.horizontalInput;
+        var speedY = 0.0f;
+        if (this.verticalInput > 0.0) {
+            speedY = LadderUpSpeed * this.verticalInput;
+        } else if (this.verticalInput < 0.0) {
+            speedY = LadderDownSpeed * this.verticalInput;
         }
 
+        if (ladderMounted) {
+            this.body.velocity = new Vector2(speedX, speedY);
+        } else {
+            this.body.velocity = new Vector2(speedX, this.body.velocity.y);
+        }
+
+        // checks if you are within 0.15 position in the Y of the ground
+        var grounded = Physics2D.OverlapCircle(groundSensor.transform.position, 0.15f, groundLayer) != null;
+
+        if (this.jumpButtonPressed && grounded) {
+            var jumpForce = new Vector2(0, this.JumpModifier);
+
+            this.body.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+
+        // checks if you are on a ladder
+        var atLadder = Physics2D.OverlapCircle(transform.position, 0.4f, ladderLayer) != null;
+
+        if (!ladderMounted && atLadder && Math.Abs(verticalInput) > 0) {
+            this.body.gravityScale = 0;
+            this.body.velocity = new Vector2(this.body.velocity.x, 0);
+            this.ladderMounted = true;
+        } else if (ladderMounted && !atLadder) {
+            this.body.gravityScale = 1;
+            this.ladderMounted = false;
+        }
+
+        Debug.Log(ladderMounted);
+        Debug.Log(speedY);
+        FeedLadderMountedIntoAnimator(ladderMounted);
         FeedGroundedIntoAnimator(grounded);
-        FeedHorizontalVelocityToAnimator(speed);
+        FeedHorizontalVelocityToAnimator(speedX);
+        FeedVerticalVelocityToAnimator(speedY);
 
         UpdateFacing();
+    }
+
+    void FeedLadderMountedIntoAnimator(bool mounted) {
+        if (this.animator != null) {
+            this.animator.SetBool("ladder", mounted);
+        }
     }
 
     void FeedGroundedIntoAnimator(bool grounded) {
@@ -60,15 +99,21 @@ public class Controller : MonoBehaviour
 
     void FeedHorizontalVelocityToAnimator(float speed) {
         if (this.animator != null) {
-            this.animator.SetFloat("speedX", System.Math.Abs(speed));
+            this.animator.SetFloat("speedX", Math.Abs(speed));
+        }
+    }
+
+    void FeedVerticalVelocityToAnimator(float speed) {
+        if (this.animator != null) {
+            this.animator.SetFloat("speedY", Math.Abs(speed));
         }
     }
 
     void UpdateFacing() {
-        if (this.rigidbody2D.velocity.x < 0 && lastDirection == 1) {
+        if (this.body.velocity.x < 0 && lastDirection == 1) {
             this.gameObject.transform.localScale = new Vector2(-1, 1);
             lastDirection = -1;
-        } else if (this.rigidbody2D.velocity.x > 0 && lastDirection == -1) {
+        } else if (this.body.velocity.x > 0 && lastDirection == -1) {
             this.gameObject.transform.localScale = new Vector2(1, 1);
             lastDirection = 1;
         }
