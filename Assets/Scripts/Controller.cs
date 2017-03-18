c161f3418857890dfca6b12fcb02c7c37c7a3b6b
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-03-18 10:02:36 +0200

    Basic movement animations completed.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,11 +7,17 @@ public class Controller : MonoBehaviour
     private float horizontalInput;
     private Rigidbody2D rigidbody2D;
     private bool jumpButtonPressed;
+    private Animator animator;
+    private int lastDirection = 1;
+    private Transform groundSensor;
+    public LayerMask groundLayer;
 
     // Use this for initialization
 	private void Start ()
 	{
+        this.animator = this.gameObject.GetComponent<Animator>();
 	    this.rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+        this.groundSensor = this.gameObject.transform.FindChild("GroundSensor");
 	}
 	
 	// Update is called once per frame
@@ -27,13 +33,45 @@ public class Controller : MonoBehaviour
     {
         var speed = this.MaxSpeed * this.horizontalInput;
         var moveSpeed = new Vector2(speed, this.rigidbody2D.velocity.y);
+        // checks if you are within 0.15 position in the Y of the ground
+        var grounded = Physics2D.OverlapCircle(groundSensor.transform.position, 0.15f, groundLayer) != null;
+        Debug.Log(grounded);
+
+
         this.rigidbody2D.velocity = moveSpeed;
 
-        if(this.jumpButtonPressed)
+        if (this.jumpButtonPressed && grounded)
         {
             var jumpForce = new Vector2(0, this.JumpModifier);
 
             this.rigidbody2D.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+
+        FeedGroundedIntoAnimator(grounded);
+        FeedHorizontalVelocityToAnimator(speed);
+
+        UpdateFacing();
+    }
+
+    void FeedGroundedIntoAnimator(bool grounded) {
+        if (this.animator != null) {
+            this.animator.SetBool("grounded", grounded);
+        }
+    }
+
+    void FeedHorizontalVelocityToAnimator(float speed) {
+        if (this.animator != null) {
+            this.animator.SetFloat("speedX", System.Math.Abs(speed));
+        }
+    }
+
+    void UpdateFacing() {
+        if (this.rigidbody2D.velocity.x < 0 && lastDirection == 1) {
+            this.gameObject.transform.localScale = new Vector2(-1, 1);
+            lastDirection = -1;
+        } else if (this.rigidbody2D.velocity.x > 0 && lastDirection == -1) {
+            this.gameObject.transform.localScale = new Vector2(1, 1);
+            lastDirection = 1;
         }
     }
 }
